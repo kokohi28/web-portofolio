e7e2de995b3cea99bf2b99ee6394e1199e0a75ac
kokohi28 <himawan.koko@gmail.com>
2020-05-24 19:56:38 +0700

    [+] Init files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Koko `CO2` Himawan Permadi Web Portofolio
+Front-end : Use HTML, CSS and Javascript
+Back-end : Nginx, serve static files
+
+### Student
+Informatic Engineering of State University of Surabaya (UNESA)
+- Koko Himawan Permadi (19051204111)
+
+## Preview
+Web Portofolio
+![Web Portofolio](https://github.com/kokohi28/web-portofolio/blob/master/home_sample.png?raw=true)
+
+
---
+++ js/script.js
@@ -0,0 +1,77 @@
+window.addEventListener("load", function() {
+  document.querySelector(".preloader").classList.add("opacity-0");
+  setTimeout(function() {
+    document.querySelector(".preloader").style.display = "none";
+  }, 1000);
+});
+
+// Aside navbar
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length,
+      allSection = document.querySelectorAll(".section"),
+      totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+  let a = navList[i].querySelector("a");
+  a.addEventListener("click", function() {
+    // remove back-section
+    for (let k = 0; k < totalSection; k++) {
+      allSection[k].classList.remove("back-section");
+    }
+
+    for (let j = 0; j < totalNavList; j++) {
+      // add back-section
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
+
+      navList[j].querySelector("a").classList.remove("active");
+    }
+
+    this.classList.add("active");
+    showSection(this);
+
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
+    }
+  });
+}
+
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  let target = element.getAttribute("href").split('#')[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+
+// Menu toggler
+const menuTogglerBtn = document.querySelector(".menu-toggler"),
+      aside = document.querySelector(".aside");
+
+menuTogglerBtn.addEventListener("click", asideSectionTogglerBtn);
+
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  menuTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
+
+// Email Me
+document.querySelector(".email-me").addEventListener("click", function() {
+  // let idx = this.getAttribute("data-section-index");
+  // console.log(idx);
+  funcEmailMe();
+});
+
+function funcEmailMe() {
+  let mailAddr = 'himawan.koko@gmail.com';
+  let mailSubject = 'Interesting%32in%32CV';
+  let mailBody = 'Hi%32Koko…';
+  let mailStr = `mailto:${mailAddr}?subject=${mailSubject}&body=${mailBody}`; 
+  console.log(mailStr)
+  window.open(mailStr);
+}
